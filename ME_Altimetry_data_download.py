f029f4071ecb41d9d8e88c21c0e3782565cef220
wlchen88 <113431383+wlchen88@users.noreply.github.com>
2024-01-10 11:15:15 +0000

    Update ME_Altimetry_data_download.py

--- ME_Altimetry_data_download.py
+++ ME_Altimetry_data_download.py
@@ -8,9 +8,9 @@ import os
 auth = earthaccess.login(strategy="netrc")
 print('login success')
 
-os.system('aws s3 ls s3://podaac-ops-cumulus-protected/SEA_SURFACE_HEIGHT_ALT_GRIDS_L4_2SATS_5DAY_6THDEG_V_JPL2205/')
-# s3 = s3fs.S3FileSystem(anon=True)
-# s3.ls('podaac-ops-cumulus-public/SEA_SURFACE_HEIGHT_ALT_GRIDS_L4_2SATS_5DAY_6THDEG_V_JPL2205/')
+#os.system('aws s3 ls s3://podaac-ops-cumulus-protected/SEA_SURFACE_HEIGHT_ALT_GRIDS_L4_2SATS_5DAY_6THDEG_V_JPL2205/')
+s3 = s3fs.S3FileSystem(anon=False)
+s3.ls('podaac-ops-cumulus-public/SEA_SURFACE_HEIGHT_ALT_GRIDS_L4_2SATS_5DAY_6THDEG_V_JPL2205/')
 
 # results = earthaccess.search_data(
 #     short_name='SEA_SURFACE_HEIGHT_ALT_GRIDS_L4_2SATS_5DAY_6THDEG_V_JPL2205',
