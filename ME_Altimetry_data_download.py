12f3de419477f8e815cb0c8c00aa854c7907c359
wlchen88 <113431383+wlchen88@users.noreply.github.com>
2024-01-10 10:35:38 +0000

    Update ME_Altimetry_data_download.py

--- ME_Altimetry_data_download.py
+++ ME_Altimetry_data_download.py
@@ -4,15 +4,13 @@ import xarray as xr
 
 #Authentication with Earthdata Login
 auth = earthaccess.login(strategy="netrc")
-print('Hello')
 print('login success')
-
 
 results = earthaccess.search_data(
     short_name='SEA_SURFACE_HEIGHT_ALT_GRIDS_L4_2SATS_5DAY_6THDEG_V_JPL2205',
     cloud_hosted=True,
     bounding_box=(-105, -40, 0, 50),
-    temporal=("2023-01", "2023-02"),
+    temporal=("2022-01", "2022-02"),
     count=10
 )
 files = earthaccess.download(results, "/home/ubuntu/SSHAlt")
