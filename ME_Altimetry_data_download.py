71550bf729478ccaaf66a7a67f6b42d5b94f9783
wlchen88 <113431383+wlchen88@users.noreply.github.com>
2024-01-10 10:45:50 +0000

    Update ME_Altimetry_data_download.py

--- ME_Altimetry_data_download.py
+++ ME_Altimetry_data_download.py
@@ -1,16 +1,20 @@
 #Import packages
 import earthaccess
 import xarray as xr
+import s3fs
 
 #Authentication with Earthdata Login
 auth = earthaccess.login(strategy="netrc")
 print('login success')
 
-results = earthaccess.search_data(
-    short_name='SEA_SURFACE_HEIGHT_ALT_GRIDS_L4_2SATS_5DAY_6THDEG_V_JPL2205',
-    cloud_hosted=True,
-    bounding_box=(-105, -40, 0, 50),
-    temporal=("2022-01", "2022-02"),
-    count=10
-)
-files = earthaccess.download(results, "/home/ubuntu/SSHAlt")
+s3 = s3fs.S3FileSystem(anon=True)
+s3.ls('podaac-ops-cumulus-public/SEA_SURFACE_HEIGHT_ALT_GRIDS_L4_2SATS_5DAY_6THDEG_V_JPL2205/')
+
+# results = earthaccess.search_data(
+#     short_name='SEA_SURFACE_HEIGHT_ALT_GRIDS_L4_2SATS_5DAY_6THDEG_V_JPL2205',
+#     cloud_hosted=True,
+#     bounding_box=(-105, -40, 0, 50),
+#     temporal=("2022-01", "2022-02"),
+#     count=10
+# )
+# files = earthaccess.download(results, "/home/ubuntu/SSHAlt")
